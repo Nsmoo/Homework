ec74d618a604a3c7e5e2134e48788391cba9c911
NikitaAdamov <nickadamow@gmail.com>
2024-05-22 16:31:40 +0300

    проверка

--- server.c
+++ server.c
@@ -15,7 +15,7 @@
 #include <per_decoder.h>
 
 #define PORT 8080 
-// fgerg
+// 111111111111111111111111111111111111
 void log_message(const char *msg) {
     time_t now = time(NULL);
     struct tm *t = localtime(&now);
