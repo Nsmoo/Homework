2e244a9d1d00e9a6f0b5c404d11cc2ac9a80e783
NikitaAdamov <nickadamow@gmail.com>
2024-05-22 15:46:49 +0300

    qwer

--- server.c
+++ server.c
@@ -14,8 +14,8 @@
 #include <per_encoder.h>
 #include <per_decoder.h>
 
-#define PORT 8080
-
+#define PORT 8080 
+// fgergr
 void log_message(const char *msg) {
     time_t now = time(NULL);
     struct tm *t = localtime(&now);
