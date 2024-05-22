0a17a73ecc10a328151475513140a2b673743d09
NikitaAdamov <nickadamow@gmail.com>
2024-05-22 16:00:29 +0300

    тест

--- server.c
+++ server.c
@@ -14,7 +14,7 @@
 #include <per_encoder.h>
 #include <per_decoder.h>
 
-#define PORT 8080 //gfrtegrf
+#define PORT 8080 //234566453
 
 void log_message(const char *msg) {
     time_t now = time(NULL);
